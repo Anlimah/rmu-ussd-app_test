828451f9e4cc83a52c3536488d6bb053c2656f71
Anlimah <y.m.ratty7@gmail.com>
2023-11-09 11:00:13 +0000

    Working async payment process 1

--- processPayment.php
+++ processPayment.php
@@ -6,16 +6,12 @@ use Predis\Client;
 use Src\Controller\PaymentController;
 
 $redis = new Client();
-
-// Define the Redis channel name
 $channelName = 'payment_channel';
 
-// Subscribe to the Redis channel
 $redis->pubSubLoop(function ($event) {
     if ($event->kind === 'message') {
         $paymentData = json_decode($event->payload, true);
-        (new PaymentController())->orchardPaymentControllerB($paymentData);
+        echo $paymentData;
+        //(new PaymentController())->orchardPaymentControllerB($paymentData);
     }
 }, $channelName);
-
-// The script will keep running and listening for messages until manually stopped.
