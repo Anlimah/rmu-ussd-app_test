18f0011e4c4782e9fa6fca76e44c262f100b492a
Anlimah <y.m.ratty7@gmail.com>
2023-11-10 16:49:55 +0000

    Added changed processPayment to a python module and corrected test.json data

---
+++ processPayment.py
@@ -0,0 +1,22 @@
+import json
+import redis
+import requests
+
+channel_name = 'payment_channel'
+php_script_url = 'http://your-server/your-script.php'
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+pubsub = redis_client.pubsub()
+pubsub.subscribe(channel_name)
+
+print("Started...")
+for message in pubsub.listen():
+    print("Listening...", end=" ")
+    if message['type'] == 'message':
+        payment_data = json.loads(message['data'])
+        print("\n", json.dumps(payment_data))
+        response = requests.post(php_script_url, data={'payment_data': json.dumps(payment_data)})
+        if response.status_code == 200:
+            print("Payment data sent successfully.")
+        else:
+            print("Error sending payment data:", response.text)
