b2d3bdd3900639b01d7acf685712e8a97d2776a5
Anlimah <y.m.ratty7@gmail.com>
2023-11-10 18:18:52 +0000

    Added pay url

--- processPayment.py
+++ processPayment.py
@@ -3,7 +3,7 @@ import redis
 import requests
 
 channel_name = 'payment_channel'
-target_url = 'https://test.pay.rmuictonline.com'
+target_url = 'https://pay.rmuictonline.com'
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 pubsub = redis_client.pubsub()
@@ -14,9 +14,18 @@ for message in pubsub.listen():
     print("Listening...", end=" ")
     if message['type'] == 'message':
         payment_data = json.loads(message['data'])
-        print("\n", json.dumps(payment_data))
         response = requests.post(target_url, data=json.dumps(payment_data))
         if response.status_code == 200:
-            print("Payment data sent successfully.")
+            print("Status: Success", {
+                "pay_category":payment_data["pay_category"],
+                "country_code":payment_data['country_code'],
+                "phone_number":payment_data["phone_number"],
+                "pay_method":payment_data["pay_method"],
+                "amount":payment_data["amount"]})
         else:
-            print("Error sending payment data:", response.text)
+            print("Status: Error",{
+                "pay_category":payment_data["pay_category"],
+                "country_code":payment_data['country_code'],
+                "phone_number":payment_data["phone_number"],
+                "pay_method":payment_data["pay_method"],
+                "amount":payment_data["amount"]})
