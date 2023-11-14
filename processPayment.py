c3645527dabe83ece7140c9bf0ab50f02b54cbb9
Anlimah <y.m.ratty7@gmail.com>
2023-11-14 18:08:38 +0000

    Logging paymentChannel data 3

--- processPayment.py
+++ processPayment.py
@@ -1,6 +1,10 @@
 import json
 import redis
 import requests
+import logging
+
+# Set your desired logging level
+logging.basicConfig(level=logging.INFO)
 
 channel_name = 'paymentChannel'
 target_url = 'https://pay.rmuictonline.com'
@@ -9,23 +13,17 @@ redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 pubsub = redis_client.pubsub()
 pubsub.subscribe(channel_name)
 
-print("Started...")
+logging.info("Started...")
+
 for message in pubsub.listen():
-    print("Listening...", end=" ")
+    logging.info("Listening...")
+
     if message['type'] == 'message':
         payment_data = json.loads(message['data'])
-        response = requests.post(target_url, data=json.dumps(payment_data))
-        if response.status_code == 200:
-            print("Status: Success", {
-                "pay_category":payment_data["pay_category"],
-                "country_code":payment_data['country_code'],
-                "phone_number":payment_data["phone_number"],
-                "pay_method":payment_data["pay_method"],
-                "amount":payment_data["amount"]})
-        else:
-            print("Status: Error",{
-                "pay_category":payment_data["pay_category"],
-                "country_code":payment_data['country_code'],
-                "phone_number":payment_data["phone_number"],
-                "pay_method":payment_data["pay_method"],
-                "amount":payment_data["amount"]})
+
+        try:
+            response = requests.post(target_url, data=json.dumps(payment_data))
+            response.raise_for_status()
+            logging.info("Status: Success %s", payment_data)
+        except requests.exceptions.RequestException as e:
+            logging.error("Status: Error %s. Error message: %s", payment_data, str(e))
