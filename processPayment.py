300fdff5b84c6dc3866afc14f1728abd2154c5aa
Anlimah <y.m.ratty7@gmail.com>
2023-11-14 17:15:54 +0000

    Logging data into logs 5

--- processPayment.py
+++ processPayment.py
@@ -9,6 +9,7 @@ redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 pubsub = redis_client.pubsub()
 pubsub.subscribe(channel_name)
 
+print("Started...")
 for message in pubsub.listen():
     print("Listening...", end=" ")
     if message['type'] == 'message':
