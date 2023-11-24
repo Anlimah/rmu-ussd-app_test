7e1c90ec85fa5f5012ef6949a0d532f9db71b367
Anlimah <y.m.ratty7@gmail.com>
2023-11-24 12:22:31 +0000

    Added .htaccess and changed orchard callback to test.pay env

--- processPayment.py
+++ processPayment.py
@@ -8,17 +8,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 channel_name = 'paymentChannel'
-target_url = 'https://pay.rmuictonline.com/pay'
+target_url = 'https://test.pay.rmuictonline.com/pay'
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 pubsub = redis_client.pubsub()
 pubsub.subscribe(channel_name)
 
-logger.info("Started...")
-
 for message in pubsub.listen():
-    logger.info("Listening...")
-
     if message['type'] == 'message':
         payment_data = json.loads(message['data'])
         headers = {'Content-Type': 'application/json'}  # Set the Content-Type header
