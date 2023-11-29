1d3a09a759de1da77f0f55c8162a7ac660401a90
Anlimah <y.m.ratty7@gmail.com>
2023-11-29 15:16:45 +0000

    Added data access timeout 2

--- index.php
+++ index.php
@@ -23,10 +23,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
         if (!empty($payData)) {
             try {
-                $redis = new Client([
-                    'host' => '127.0.0.1',
-                    'port' => 6379,
-                ]);
+                $redis = new Client(['host' => '127.0.0.1', 'port' => 6379,]);
                 $redis->publish('testPaymentChannel', json_encode($payData));
                 file_put_contents(
                     'processUSSD.log',
